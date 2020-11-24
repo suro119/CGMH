301e00eaae6953841c328541775e8b6ef6cacc77
jeongeonp <jeongeonpark1@gmail.com>
2020-11-24 20:53:06 +0900

    Add main file

---
+++ input_gen.py
@@ -0,0 +1,28 @@
+# Import
+
+
+# RNN
+
+
+# CGMH initialization: line 202-254 (수로)
+if config.mode=='use':
+      #CGMH sampling for key_gen
+
+
+
+
+# CGMH replacement: line 256-303 (정언)
+#word replacement (action: 0)
+
+
+
+
+
+# CGMH insertion: line 305-364 (수로)
+#word insertion(action:1)
+
+
+
+
+# CGMH deletion: line 367-450 (찬희)
+#word deletion(action: 2)
