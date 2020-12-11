ba27064f36ee3189c86efccaef0501e6acb2ad10
Suro Lee <sulee1415@gmail.com>
2020-12-12 01:09:20 +0900

    Add data folder and the new dict.pkl file

---
+++ data/dict_make.py
@@ -0,0 +1,22 @@
+from collections import Counter
+import pickle as pkl
+
+dict_size = 30000
+
+with open('1-billion.txt', 'r') as f:
+    sentence = f.read()
+counter = Counter(sentence.split())
+counter = counter.most_common(dict_size)
+
+words = list(zip(*counter))[0]
+
+word2id = dict(zip(words, range(dict_size)))
+id2word = dict(zip(range(dict_size), words))
+
+with open('dict.pkl', 'wb') as f:
+    pkl.dump([word2id, id2word], f)
+
+# with open('dict.pkl', 'rb') as f:
+#     data = pkl.load(f)
+#     print(data[0])
+#     print(data[1])
